61d57316cb9f755c71b9e88d6773860289dfa901
Ido Barkai <barkaido21@gmail.com>
2022-04-02 14:54:35 +0300

    added bootstrap js and validation to form

--- web-client/src/login/LoginForm.js
+++ web-client/src/login/LoginForm.js
@@ -1,9 +1,12 @@
 import { useRef } from 'react'
 import { Link } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom'
+import { useState } from 'react'
 import users from '../database/Users'
+import $ from 'jquery'
 
 function LoginForm() {
+    const [error,setError] = useState(false);
     let navigate = useNavigate();
     let usernameRef = useRef(null)
     let passwordRef = useRef(null)
@@ -15,21 +18,33 @@ function LoginForm() {
             if(user.username === usernameInput && user.password === passwordInput){
                 navigate('/chat');
             }
+            else{
+                setError(true);
+            }
         }
+    }
+
+    var errorMessage;
+    if (error){
+        errorMessage = <div class="alert alert-danger d-flex align-items-center" role="alert">
+                            <i class="bi-exclamation-octagon-fill mx-2"></i>
+                            Invalid Username or Password
+                        </div>;
     }
 
     return (
         <div>
         <div className="mb-3">
               <label htmlFor="inputUsername" className="form-label">Username</label>
-              <input className="form-control" ref={usernameRef}></input>
+              <input className="form-control" ref={usernameRef} id="username" required></input>
         </div>
-            <div className="mb-3">
+            <div className="mb-3" id="password">
               <label htmlFor="inputPassword" className="form-label">Password</label>
-              <input type="password" className="form-control" ref={passwordRef}></input>
+              <input type="password" className="form-control" ref={passwordRef} required></input>
             </div>
             <p className="form-label">Dont have a username? <Link to="/signup">click here to sign up</Link></p>
             <button className="btn btn-success" onClick={doLogin}>Submit</button>
+            {errorMessage}
         </div>
     );
 }
