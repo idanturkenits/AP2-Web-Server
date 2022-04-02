d582f485c6bceb665756d32de2dbcdfa1dc5e7b9
Ido Barkai <barkaido21@gmail.com>
2022-04-02 12:55:53 +0300

    added User style

--- web-client/src/chat/User.js
+++ web-client/src/chat/User.js
@@ -13,13 +13,16 @@ class User extends React.Component {
       return (
           <div class="card mb-3" styles="max-width: 540px;">
               <div class="row no-gutters">
-                  <div class="col-md-4">
-                      <img src="..." class="card-img" alt="..."></img>
+                  <div class="col-md-4 my-auto">
+                      <img src="logo512.png" class="card-img rounded" alt="..."></img>
                   </div>
-                  <div class="col-md-8">
-                      <div class="card-body">
-                          <h5 class="card-title">{this.state.name}</h5>
-                          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                  <div class="col-md-8 my-auto">
+                      <div class="card-body justify-content-between d-flex">
+                          <div class="ms-2 me-auto">
+                              <h5 class="card-title fw-bold">{this.state.name}
+                              </h5>
+                              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                          </div>
                       </div>
                   </div>
               </div>
