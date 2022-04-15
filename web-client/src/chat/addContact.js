a5e38a8575c6ea54930399b71eb4381c7d265bba
Idan <idantur06@gmail.com>
2022-04-15 14:17:28 +0300

    changed form to div

--- web-client/src/chat/addContact.js
+++ web-client/src/chat/addContact.js
@@ -49,13 +49,13 @@ function AddContact(props) {
                             <button type="button" id="closeAddContact" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div class="modal-body">
-                            <form id="addContactForm">
+                            <div id="addContactForm">
                                 <div class="form-row">
                                     <div class="form-group mb-2">
                                         <input type="text" onKeyUp={sendByEnter} class="form-control" ref={usernameRef} placeholder="Username"></input>
                                     </div>
                                 </div>
-                            </form>
+                            </div>
                         </div>
                         <p className="form-label ms-2 mt-2 mb-5 text-danger">{error}</p>
                         <div class="modal-footer justify-content-start">
