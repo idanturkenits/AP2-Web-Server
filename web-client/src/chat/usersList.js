c94d189d75e59682393cd6873ac12636c441a171
idob10 <barkaido21@gmail.com>
2022-04-16 15:48:27 +0300

    sorted usersList

--- web-client/src/chat/usersList.js
+++ web-client/src/chat/usersList.js
@@ -3,7 +3,15 @@ import React from 'react'
 import ChatCard from "./ChatCard"
 
 function UsersList({currentUser, chats, displayChat}){
-    const usersList = chats.map((chat,key) => {
+    let sortFunc = function(a,b) {
+        if (b.messages.length==0)
+            return -1;
+        if (a.messages.length==0)
+            return 1;
+        return a.messages[a.messages.length-1].date>=b.messages[b.messages.length-1].date?-1:1;
+    }
+
+    const usersList = chats.sort(sortFunc).map((chat,key) => {
         return (
             <ChatCard currentUser={currentUser} chat={chat} displayChat={displayChat}/>
         );
