d00e33bf2b9074bbaff7800f5ff4fc1c2cb916dd
Yogev173 <a0585324482@gmail.com>
2022-05-06 13:12:42 +0300

    added react code

---
+++ serverSide/chatReact/React-Client/Modles/Rating.cs
@@ -0,0 +1,15 @@
+﻿namespace React_Client.Modles
+{
+    public class Rating
+    {
+        [Key]
+        public string Name { get; set; }
+
+        public string Explanation { get; set; }
+
+
+        [Range(1, 5,
+            ErrorMessage = "Value for {0} must be between {1} and {2}.")]
+        public int Rate { get; set; }
+    }
+}
