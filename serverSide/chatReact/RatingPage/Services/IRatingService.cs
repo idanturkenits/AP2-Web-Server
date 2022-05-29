01e30fbf4f8cd0bfb0b57766fa9e4ebbcf3d2ace
Idan Turkenits <idanturt06@gmail.com>
2022-05-29 19:55:54 +0300

    added service

---
+++ serverSide/chatReact/RatingPage/Services/IRatingService.cs
@@ -0,0 +1,18 @@
+﻿using RatingPage.Models;
+
+namespace RatingPage.Services
+{
+    public interface IRatingService
+    {
+        Task<List<Rating>> getAllRatings();
+
+        Task<Rating> Get(int id);
+
+        Task Add(Rating rating);
+
+        Task Remove(int id);
+
+        Task Edit(Rating rating);
+
+    }
+}
---
+++ serverSide/chatReact/RatingPage/Services/RatingService.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RatingPage.Data;
+using RatingPage.Models;
+
+namespace RatingPage.Services
+{
+    public class RatingService : IRatingService
+    {
+        private RatingPageContext _context;
+        public RatingService(RatingPageContext context)
+        {
+            _context = context;
+        }
+        public async Task Add(Rating rating)
+        {
+            _context.Add(rating);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Edit(Rating rating)
+        {
+            _context.Update(rating);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<Rating> Get(int id)
+        {
+            var rating = await _context.Rating.FirstOrDefaultAsync(m => m.Id == id);
+            return rating;
+        }
+
+        public async Task<List<Rating>> getAllRatings()
+        {
+            List<Rating> ratings = await _context.Rating.ToListAsync();
+            return ratings;
+        }
+
+        public async Task Remove(int id)
+        {
+            var rating = await Get(id);
+            _context.Rating.Remove(rating);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
